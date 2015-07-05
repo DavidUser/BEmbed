44f3f876b458ab5a47daceb9c557b4488b169973
David Kennedy S. A <daviduser.guj@gmail.com>
2015-07-05 19:18:47 -0300

    blink example

---
+++ examples/blink/main.cpp
@@ -0,0 +1,17 @@
+/*
+   Blink
+   Turns LED on and off by one second, repeatedly.
+*/
+
+#include <digitalOutput.h>
+
+main() {
+	Pin led(13); //pin 13 are represented by this object named led
+	
+	while(true) { //do this repeatedly forever
+		led.turnOn(); // put high voltage on pin
+		delay(1_seconds); //1 second delay, using user-defined literals (C++11)
+		led.turnOff(); // put low voltage on pin
+		delay(1_seconds); //1 second delay
+	}
+}
