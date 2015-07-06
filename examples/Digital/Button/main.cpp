25e04c584ec956b52c198aa9361c2406dd174e50
David Kennedy S. A <daviduser.guj@gmail.com>
2015-07-06 12:19:43 -0300

    Button (with interruption) example

---
+++ examples/Digital/Button/main.cpp
@@ -0,0 +1,33 @@
+/*
+   Button
+   Turns on and off a light emitting diode (LED) connected to digital
+   pin 13, when pressing a bushbutton attached to pin 2.
+
+   The circuit:
+   * LED attached from pin 13 to ground
+   * pushbutton attached to pin 2 from +5V
+   * 10K resistor attached to pin 2  from ground
+
+   * Note: on most Arduinos there is already an LED on the board
+   attached to pin 13
+   */
+
+#include <digitalIO.h>
+#include <interruption.h>
+
+Pin button(2); // object button represent the digital pin 2
+Pin led(13); // object led represent the digital pin 13
+
+// function called when a pin change your state
+void buttonStateToLED() {
+	led.toggle(); // toggle the led state
+}
+
+main() {
+	/* An interruption stop the normal working of microcontroller and run another work,
+	   in this case every change on pin represented by button will stop the normal working of microcontroller
+	   and call the buttonStateToLED() function, then it'll return to the normal work until the next change
+	   on pin represented by button
+	   */
+	SignalInterruption buttonPress(TOGGLE, button, buttonStateToLED); // buttonPress object represent a hardware interruption detecting toggle on pin button and call the buttonStateToLED() function
+}
