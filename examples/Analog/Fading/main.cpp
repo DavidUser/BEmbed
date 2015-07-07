79b17dc58c69da92e4013d6a1775903538acf047
David Kennedy S. A <daviduser.guj@gmail.com>
2015-07-07 19:57:01 -0300

    Fading example

---
+++ examples/Analog/Fading/main.cpp
@@ -0,0 +1,24 @@
+/*
+   Fading
+   
+   This example shows how to fade an LED using the pwm writing
+
+   The circuit:
+   * LED attached from digital pin 9 to ground
+   */
+
+#include "analogOutput.h"
+
+main() {
+	PWMPin led(9); // LED connected to digital pin 9
+
+	while (true) { // repeat forever
+		// increasing brightness
+		for (unsigned fadeValue = led.getMinLimit(); fadeValue <= led.getMaxLimit(); fadeValue += 5)
+			led.write(fadeValue), delay(30_milliseconds); // set led brigthness and wait 30ms
+		// decreasing brigthness
+		for (unsigned fadeValue = led.getMaxLimit(); fadeValue >= led.getMinLimit(); fadeValue -= 5)
+			led.write(fadeValue), delay(30_milliseconds); // set led brigthness and wait 30ms
+	}
+}
+	
