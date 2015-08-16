55b9c2bd4845535de6b536d211c7e8d946a76e60
David Kennedy S. A <daviduser.guj@gmail.com>
2015-08-16 12:23:20 -0300

    Analog pin interface

--- src/zeus/interfaces/AnalogPin.h
+++ src/zeus/interfaces/AnalogPin.h
@@ -1,12 +1,14 @@
+#pragma once
+
+#include "InputPin.h"
+
 namespace zeus {
 	namespace interfaces {
 		class AnalogPin : zeus::interfaces::InputPin {
 		public:
-			virtual unsigned long long read() = 0;
 			virtual void setResolution(zeus::platforms::PlatformFactory::AnalogResolution resolution) = 0;
-			virtual Resolution getResolution() = 0;
-			virtual std::pair<int, int> getLimits() = 0;
-			virtual void setResolution(zeus::platforms::PlatformFactory::AnalogResolution resolution) = 0;
+			virtual Resolution getResolution() const = 0;
+			virtual std::pair<int, int> getLimits() const = 0;
 		};
 	}
 }
