cac87ca762cc8768588033f3960820ceeb3f36a6
David Kennedy S. A <daviduser.guj@gmail.com>
2015-08-16 12:23:20 -0300

    AnalogPin fix necessary include

--- src/zeus/interfaces/AnalogPin.h
+++ src/zeus/interfaces/AnalogPin.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "InputPin.h"
+#include "../platforms/PlatformFactory.h"
 
 namespace zeus {
 	namespace interfaces {
@@ -8,7 +9,6 @@ namespace zeus {
 		public:
 			virtual void setResolution(zeus::platforms::PlatformFactory::AnalogResolution resolution) = 0;
 			virtual Resolution getResolution() const = 0;
-			virtual std::pair<int, int> getLimits() const = 0;
 		};
 	}
 }
