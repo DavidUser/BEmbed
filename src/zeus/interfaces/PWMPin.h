ff5b9a59bab43c92c5907c9e5ffd9add89fbeb9a
David Kennedy S. A <daviduser.guj@gmail.com>
2015-08-17 16:53:48 -0300

    PWM pin interface

--- src/zeus/interfaces/PWMPin.h
+++ src/zeus/interfaces/PWMPin.h
@@ -1,14 +1,19 @@
+#pragma once
+
+#include "OutputPin.h"
+#include "Time.h"
+
 namespace zeus {
 	namespace interfaces {
 		class PWMPin : zeus::interfaces::OutputPin {
 		public:
 			virtual void setPulseWidth(Time width) = 0;
-			virtual Time getPulseWidth() = 0;
+			virtual Time getPulseWidth() const = 0;
 			virtual void turnOn() = 0;
 			virtual void turnOff() = 0;
 			virtual void setMode(PWMMode mode) = 0;
-			virtual PWMMode getMode() = 0;
-			virtual std::pair<int, int> getLimits() = 0;
+			virtual PWMMode getMode() const = 0;
+			virtual const std::pair<int, int> & getLimits() const = 0;
 		};
 	}
 }
---
+++ src/zeus/interfaces/Time.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//TODO Can include minute, hour, day and another time measures inclusive clocks can be very useful
+
+namespace zeus {
+	using Time = unsigned long long;
+
+	constexpr Time millisecond = 1;
+	constexpr Time second = 1000 * millisecond;
+}
+
+constexpr zeus::Time operator"" _miliseconds(const zeus::Time miliseconds) {
+	return miliseconds * zeus::millisecond;
+}
+
+constexpr zeus::Time operator"" _seconds(const zeus::Time seconds) {
+	return seconds * zeus::second;
+}
