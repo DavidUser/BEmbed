1537b6d108d6bfcd00f889942ba477ec430efdb0
David Kennedy S. A <daviduser.guj@gmail.com>
2015-08-17 21:25:46 -0300

    Digital pin interface

--- src/zeus/interfaces/DigitalPin.h
+++ src/zeus/interfaces/DigitalPin.h
@@ -1,16 +1,20 @@
+#pragma once
+
+#include "IOPin.h"
+#include <utility>
+
 namespace zeus {
 	namespace interfaces {
 		class DigitalPin : zeus::interfaces::IOPin {
 		public:
-			bool HIGH;
-			bool LOW;
+			static const bool HIGH = true;
+			static const bool LOW = false;
 
-			virtual void setState(bool state) = 0;
-			virtual bool getState() = 0;
+			virtual void setState(const bool state) = 0;
+			virtual bool getState() const = 0;
 			virtual void turnHigh() = 0;
 			virtual void turnLow() = 0;
 			virtual void toogle() = 0;
-			virtual std::pair<int, int> getLimits() = 0;
 		};
 	}
 }
