f1fa92e232577a610c6c262d8622c4b3eb0fe3d4
David Kennedy S. A <daviduser.guj@gmail.com>
2015-08-19 17:03:34 -0300

    Timer interface representation

--- src/zeus/interfaces/Timer.h
+++ src/zeus/interfaces/Timer.h
@@ -1,15 +1,20 @@
+#pragma once
+
+#include "Time.h"
+#include "../platforms/PlatformFactory.h"
+
 namespace zeus {
 	namespace interfaces {
 		class Timer {
 		public:
-			void configureInterval(zeus::Time time);
-			void configureMode(TimerMode mode);
-			void start();
-			void stop();
-			zeus::platforms::PlatformFactory::InterruptionEvent getInterruptionEvent();
-			unsigned long long getMaxLimit();
-			unsigned long long getCurrent();
-			unsigned long long getClocksBy(zeus::Time time);
+			virtual void configureInterval(const zeus::Time time) = 0;
+			virtual void configureMode(const platforms::PlatformFactory::TimerMode mode) = 0;
+			virtual void start() = 0;
+			virtual void stop() = 0;
+			virtual zeus::platforms::PlatformFactory::InterruptionEvent getInterruptionEvent() const = 0;
+			virtual constexpr unsigned long long getMaxLimit() const noexcept = 0;
+			virtual unsigned long long getCurrent() const = 0;
+			virtual static constexpr unsigned long long getClocksBy(const zeus::Time time) noexcept = 0;
 		};
 	}
 }
