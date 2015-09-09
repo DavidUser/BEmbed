09124a8eaed34f8e2ad919ecdf7e808e5cf23f65
David Kennedy S. A <daviduser.guj@gmail.com>
2015-09-09 10:08:58 -0300

    Communication specialization interfaces

--- src/zeus/interfaces/I2c.h
+++ src/zeus/interfaces/I2c.h
@@ -1,8 +1,9 @@
+#pragma once
+
 namespace zeus {
 	namespace interfaces {
 		class I2c : zeus::interfaces::Communication {
 		public:
-			virtual void connect(unsigned address, Mode mode) = 0;
 			virtual void connect(unsigned address, Mode mode) = 0;
 
 			enum Mode {
--- src/zeus/interfaces/Spi.h
+++ src/zeus/interfaces/Spi.h
@@ -1,3 +1,5 @@
+#pragma once
+
 namespace zeus {
 	namespace interfaces {
 		class Spi : zeus::interfaces::Communication {
