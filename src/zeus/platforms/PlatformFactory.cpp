7ee6a595c4be777a421624cf56f227de305fbd5a
David Kennedy S. A <daviduser.guj@gmail.com>
2015-09-10 19:22:03 -0300

    Platform factory get instance of correct platform

--- src/zeus/platforms/PlatformFactory.cpp
+++ src/zeus/platforms/PlatformFactory.cpp
@@ -1,7 +1,10 @@
 #include "PlatformFactory.h"
 
 static zeus::platforms::PlatformFactory& zeus::platforms::PlatformFactory::getInstance() {
-	//TODO verify avalible hardware
-	static PlatformFactory instance;
+#ifndef __ZEUS_PLATFORM__
+#error "Define a platform name before use Platform Factory"
+#else
+	static zeus::platforms::__ZEUS_PLATFORM__::PlatformFactory instance;
 	return instance;
+#endif
 }
