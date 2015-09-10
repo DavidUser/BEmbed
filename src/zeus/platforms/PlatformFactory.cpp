d48ed49790188b27e02ff3ae192f391fe5d83c5f
David Kennedy S. A <daviduser.guj@gmail.com>
2015-09-10 18:46:21 -0300

    Platform factory generic interface

--- src/zeus/platforms/PlatformFactory.cpp
+++ src/zeus/platforms/PlatformFactory.cpp
@@ -1,16 +1,7 @@
 #include "PlatformFactory.h"
 
-std::unique_ptr<zeus::platforms::msp430g2553::Timer> zeus::platforms::PlatformFactory::getTimer(Timers timerId, Timers timerId) {
-	// TODO - implement PlatformFactory::getTimer
-	throw "Not yet implemented";
-}
-
-std::unique_ptr<zeus::interfaces::Interruption> zeus::platforms::PlatformFactory::getInterruption(zeus::platforms::PlatformFactory::InterruptionEvent event) {
-	// TODO - implement PlatformFactory::getInterruption
-	throw "Not yet implemented";
-}
-
 static zeus::platforms::PlatformFactory& zeus::platforms::PlatformFactory::getInstance() {
-	// TODO - implement PlatformFactory::getInstance
-	throw "Not yet implemented";
+	//TODO verify avalible hardware
+	static PlatformFactory instance;
+	return instance;
 }
--- src/zeus/platforms/PlatformFactory.h
+++ src/zeus/platforms/PlatformFactory.h
@@ -1,27 +1,54 @@
+#pragma once
+
+#include "../interfaces/DigitalPin.h"
+#include "../interfaces/AnalogPin.h"
+#include "../interfaces/PWMPin.h"
+#include "../interfaces/PulsePin.h"
+#include "../interfaces/Communication.h"
+#include "../interfaces/Timer.h"
+#include "../interfaces/Interruption.h"
+#include <memory>
+
 namespace zeus {
 	namespace platforms {
 		class PlatformFactory {
-		public:
-			virtual std::unique_ptr<zeus::interfaces::DigitalPin> getDigitalPin(unsigned number) = 0;
-			virtual std::unique_ptr<zeus::interfaces::AnalogPin> getAnalogPin(unsigned number) = 0;
-			virtual std::unique_ptr<zeus::interfaces::PWMPin> getPWMPin(unsigned number) = 0;
-			virtual std::unique_ptr<zeus::interfaces::PulsePin> getPulsePin(unsigned number) = 0;
-			virtual std::unique_ptr<zeus::interfaces::Communication> getSerialCommunication(zeus::platforms::PlatformFactory::CommunicationChannel channel) = 0;
-			std::unique_ptr<zeus::platforms::msp430g2553::Timer> getTimer(Timers timerId);
-			std::unique_ptr<zeus::interfaces::Interruption> getInterruption(zeus::platforms::PlatformFactory::InterruptionEvent event);
-			static zeus::platforms::PlatformFactory& getInstance();
-			std::unique_ptr<zeus::platforms::msp430g2553::Timer> getTimer(Timers timerId);
+			public:
+				enum class CommunicationChannel;
+				enum class AnalogResolution;
+				enum class InterruptionEvent;
+				enum class Timers;
+				enum class TimerMode;
 
-			enum CommunicationChannel {
-			};
-			enum AnalogResolution {
-			};
-			enum InterruptionEvent {
-			};
-			enum Timers {
-			};
-			enum TimerMode {
-			};
+			private:
+				PlatformFactory() = default;
+			public:
+				virtual std::unique_ptr<zeus::interfaces::DigitalPin> getDigitalPin(unsigned number) = 0;
+				virtual std::unique_ptr<zeus::interfaces::AnalogPin> getAnalogPin(unsigned number) = 0;
+				virtual std::unique_ptr<zeus::interfaces::PWMPin> getPWMPin(unsigned number) = 0;
+				virtual std::unique_ptr<zeus::interfaces::PulsePin> getPulsePin(unsigned number) = 0;
+				virtual std::unique_ptr<zeus::interfaces::Communication> getSerialCommunication(zeus::platforms::PlatformFactory::CommunicationChannel channel) = 0;
+				virtual std::unique_ptr<zeus::interfaces::Timer> getTimer(Timers timerId) = 0;
+				virtual std::unique_ptr<zeus::interfaces::Interruption> getInterruption(zeus::platforms::PlatformFactory::InterruptionEvent event) = 0;
+				static zeus::platforms::PlatformFactory& getInstance();
 		};
 	}
 }
+
+#include <functional>
+#include <type_traits>
+
+#define HASH_ZEUS_ENUM_OVERLOAD (ZEUS_ENUM) 												\
+namespace std {																\
+	template<>															\
+		struct hash<ZEUS_ENUM> {												\
+			size_t operator()(const ZEUS_ENUM& e) const { 									\
+				return hash< underlying_type<ZEUS_ENUM>::type >()(static_cast< underlying_type<ZEUS_ENUM>::type >(e)); 	\
+			}														\
+		};															\
+}
+
+HASH_ZEUS_ENUM_OVERLOAD(zeus::platforms::CommunicationChannel)
+HASH_ZEUS_ENUM_OVERLOAD(zeus::platforms::AnalogResolution)
+HASH_ZEUS_ENUM_OVERLOAD(zeus::platforms::InterruptionEvent)
+HASH_ZEUS_ENUM_OVERLOAD(zeus::platforms::Timers)
+HASH_ZEUS_ENUM_OVERLOAD(zeus::platforms::TimersMode)
