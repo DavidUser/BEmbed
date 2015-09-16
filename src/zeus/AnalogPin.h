5c34d512b4fc71feb823c6dfe0bf666254492328
David Kennedy S. A <daviduser.guj@gmail.com>
2015-09-16 19:56:52 -0300

    Analog pin concrete

--- src/zeus/AnalogPin.h
+++ src/zeus/AnalogPin.h
@@ -1,15 +1,66 @@
+#pragma once
+
+#include "interfaces/AnalogPin.h"
+#include "platforms/PlatformFactory.h"
+#include <vector>
+#include "platforms/exceptions/NoHardwareSupport.h"
+
 namespace zeus {
 	class AnalogPin : zeus::interfaces::AnalogPin {
-	public:
-		AnalogPin(unsigned pinNumber);
-		static std::vector<zeus::AnalogPin> getRange(unsigned pinNumberStart, unsigned pinNumberEnd);
-		unsigned long long read();
-		void setResolution(zeus::platforms::PlatformFactory::AnalogResolution resolution);
-		Resolution getResolution();
-		std::pair<int, int> getLimits();
-		void setNumber(unsigned number);
-		unsigned getNumber();
-	private:
-		bool compatible(unsigned pin);
+		unique_ptr<zeus::interfaces::AnalogPin> pinInstance;
+		public:
+			AnalogPin(unsigned pinNumber);
+			static std::vector<zeus::AnalogPin> getRange(unsigned pinNumberStart, unsigned pinNumberEnd);
+			inline unsigned long long read();
+			inline void setResolution(zeus::platforms::PlatformFactory::AnalogResolution resolution);
+			inline Resolution getResolution() const;
+			inline std::pair<int, int> getLimits() const;
+			inline void setNumber(unsigned number);
+			inline unsigned getNumber() const;
+		private:
+			bool compatible(unsigned pin) const;
 	};
+
+	zeus::AnalogPin::AnalogPin(unsigned pinNumber) {
+		if (compatible(pinNumber))
+			pinInstance = PlatformFactory::getInstance().getAnalogPin(pinNumber);
+		else
+			throw platforms::exception::NoHardwareSupport("No analog hardware support to this pin");
+	}
+
+	static std::vector<zeus::AnalogPin> zeus::AnalogPin::getRange(unsigned pinNumberStart, unsigned pinNumberEnd) {
+		std::vector<AnalogPin> pinsOnRange;
+		for (int i = pinNumberStart; i <= pinNumberEnd; ++i)
+			range.emplace_back(i);
+		return pinsOnRange;
+	}
+
+	unsigned long long zeus::AnalogPin::read() {
+		return pinInstance.read();
+	}
+
+	void zeus::AnalogPin::setResolution(zeus::platforms::PlatformFactory::AnalogResolution resolution) {
+		pinInstance.setResolution(resolution);
+	}
+
+	Resolution zeus::AnalogPin::getResolution() {
+		return pinInstance.getResolution();
+	}
+
+	std::pair<int, int> zeus::AnalogPin::getLimits() {
+		return pinInstance.getLimits();
+	}
+
+	void zeus::AnalogPin::setNumber(unsigned number) {
+		pinInstance.setNumber(number);
+	}
+
+	unsigned zeus::AnalogPin::getNumber() {
+		return pinInstance.getNumber();
+	}
+
+	bool zeus::analogpin::compatible(unsigned pin) {
+		return pinInstance.compatible(pin);
+	}
+
 }
